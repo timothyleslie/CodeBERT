1221ed380f037acdea6c2a1e6a45f46e7ddc5bf6
timothyleslie <836241487@qq.com>
2022-01-03 16:50:00 +0800

    change download.py

--- CodeBERT/codesearch/download.py
+++ CodeBERT/codesearch/download.py
@@ -3,9 +3,9 @@ from transformers import RobertaTokenizer, RobertaConfig, RobertaModel, RobertaF
 
 def main():
     print("loading tokenizer")
-    tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-mlm")
+    tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base-mlm")
     print("loading model")
-    model = RobertaForMaskedLM.from_pretrained("microsoft/codebert-mlm")
+    model = RobertaForMaskedLM.from_pretrained("microsoft/codebert-base-mlm")
     model_to_save = model.module if hasattr(model,
                                                 'module') else model  # Take care of distributed/parallel training
     model_to_save.save_pretrained("./models/MLM_mlm/")
